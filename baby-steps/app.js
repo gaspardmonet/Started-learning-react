60ff76ad48ee6907c0ae18d6da2ec77acc99fbd5
gaspardmonet <marianoparra1118@gmail.com>
2020-02-28 12:22:53 -0800

    2 baby step done

--- baby-steps/app.js
+++ baby-steps/app.js
@@ -1,15 +1,47 @@
-var h1 = React.createElement("h1", null, "Hello world");
-var p = React.createElement(
-  "p",
-  null,
-  "A quick bown fox jumps iver the lazy dog"
-);
-class Custom extends React.Component {
+// var h1 = React.createElement("h1", null, "Hello world");
+// var p = React.createElement(
+//   "p",
+//   null,
+//   "A quick bown fox jumps iver the lazy dog"
+// );
+// class Custom extends React.Component {
+//   render() {
+//     return React.createElement("div", null, h1, p);
+//   }
+// }
+// var what = React.createElement(Custom, null);
+// var where = document.getElementById("content");
+
+// ReactDOM.render(what, where);
+
+class CustomerLink extends React.Component {
   render() {
-    return React.createElement("div", null, h1, p);
+    console.log(`this.props ${this.props}`);
+    return React.createElement("a", this.props, this.props.linktext);
   }
 }
-var what = React.createElement(Custom, null);
+
+var myLink = React.createElement(
+  "div",
+  null,
+  React.createElement(CustomerLink, {
+    href: "https://www.google.com",
+    linktext: "google",
+    title: "google"
+  }),
+  React.createElement(CustomerLink, {
+    href: "https://www.facebook.com",
+    linktext: "facebook",
+    title: "facebook"
+  }),
+  React.createElement(CustomerLink, {
+    href: "https://www.youtube.com",
+    linktext: "youtube",
+    title: "youtube"
+  })
+);
+
+// var what = React.createElement(Custom, null);
 var where = document.getElementById("content");
 
-ReactDOM.render(what, where);
+ReactDOM.render(myLink, where);
