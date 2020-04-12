b03fe6f2b0863f8c9b078fb86a7442b17861dab3
gaspardmonet <marianoparra1118@gmail.com>
2020-04-12 20:35:27 +0500

    123

--- todo-list-react-redux/server.js
+++ todo-list-react-redux/server.js
@@ -1,8 +1,11 @@
 var express = require('express');
+var app = express();
 const path = require('path');
-
+const port = process.env.PORT || 4000;
 app.use(express.static('build'))
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'build/index.html'));
 })
+
+app.listen(port, () => console.log(`server on running on ${port}`));
